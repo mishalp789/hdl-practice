ea9cc0fd442277e30d95b3aeb1bc81ffe09dd67d
mishalp789 <mishalp789@gmail.com>
2025-05-23 23:40:01 +0530

    Day 02: OR, NOT, XOR gates in SystemVerilog with testbenches

---
+++ systemverilog/day02_basic_gate/not_gate.sv
@@ -0,0 +1,4 @@
+module not_gate(input logic a,output logic y);
+	assign y = ~a;
+endmodule
+
---
+++ systemverilog/day02_basic_gate/or_gate.sv
@@ -0,0 +1,3 @@
+module or_gate(input logic a, input logic b,output logic y);
+	assign y = a|b;
+endmodule
